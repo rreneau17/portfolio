cb4074ffbcfcb3ff184fb66d93fdd4373723dd87
Rich Reneau <rich.reneau@gmail.com>
2018-07-26 21:46:56 -0400

    added skills section

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,10 @@
 
 // popover for contact items
 $(document).ready(function(){
+    $('.fa-envelope').popover({content: "Email", trigger: "hover", placement: "bottom"});
     $('.fa-github').popover({content: "GitHub", trigger: "hover", placement: "bottom"}); 
+    $('.fa-linkedin-in').popover({content: "LinkedIn", trigger: "hover", placement: "bottom"});
+    $('.fa-file').popover({content: "Resume", trigger: "hover", placement: "bottom"});
 });
 
 // collapes the drop down navigation when item is clicked
