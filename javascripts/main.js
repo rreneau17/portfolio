9b316e3f27b7a5c98731df66b59bdf2c9b491c9d
Rich Reneau <rich.reneau@gmail.com>
2018-07-31 07:32:28 -0400

    added work around for safari in-page linking issues

--- javascripts/main.js
+++ javascripts/main.js
@@ -17,11 +17,20 @@ $('.dropdown-menu').on('click',function() {
     $('.navbar-collapse').collapse('hide');
 });
 
-// scrolls 75 pixels above section to offset the sticky top
+// scrolls 75 pixels above section to offset the fixed nav bar
 function scrollGS(aID) {
-    window.scrollTo(0,$(aID).offset().top -75) + window.innerHeight == $(document).height();
+    // needed to add this work around for safari viewports under 780.  Will research a better option.
+    if ( /^((?!chrome|android).)*safari/i.test(navigator.userAgent) && window.innerWidth < 781) {
+        window.scrollTo(0,$(aID).offset().top -335) + window.innerHeight == $(document).height();
+    } else {
+        window.scrollTo(0,$(aID).offset().top -75) + window.innerHeight == $(document).height();
+    } 
 }
-// scrolls 64 pixels above section to offset the sticky top
+// scrolls 64 pixels above section to offset the fixed nav bar
 function scrollGS2(aID) {
-    window.scrollTo(0,$(aID).offset().top -64) + window.innerHeight == $(document).height();
+    if ( /^((?!chrome|android).)*safari/i.test(navigator.userAgent) && window.innerWidth < 781) {
+        window.scrollTo(0,$(aID).offset().top -324) + window.innerHeight == $(document).height();
+    } else {
+        window.scrollTo(0,$(aID).offset().top -64) + window.innerHeight == $(document).height();
+    } 
 }
